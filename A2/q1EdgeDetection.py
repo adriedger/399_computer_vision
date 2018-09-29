2d4395d6c8d8c417d9f2a76450d39266cd03d06a
Andre <adriedge@gmail.com>
2018-09-29 16:35:05 -0600

    working on q1

--- A2/q1EdgeDetection.py
+++ A2/q1EdgeDetection.py
@@ -4,6 +4,16 @@
 import numpy as np
 import cv2 
 
+
+#k = np.array([[1, 0], [0, -1]])
+#a = np.array([[1],[-1]])
+#dot = np.multiply(a, a.T)
+#print k
+#print k.T
+#print a
+#print a.T
+#print dot
+
 #img = cv2.imread('data_q1/bsds_3096.jpg')
 img = cv2.imread('lena.tif')
 cv2.imshow('input', img)
@@ -11,6 +21,29 @@ cv2.imshow('input', img)
 def edge_detection(img, method):
 
     grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    if method == 'diff':
+        diff_x = cv2.filter2D(grey_img, -1, np.array([[0, 1], [-1, 0]]))
+        diff_y = cv2.filter2D(grey_img, -1, np.array([[1, 0], [0, -1]]))
+
+        #cv2.imshow('diff_x', diff_x)
+        #cv2.imshow('diff_y', diff_y)
+
+        #gradient_mag = diff_x^2 + diff_y^2
+        #convert to CV_32 or 64
+        #gradient_mag = cv2.sqrt(gradient_mag)
+        #convert back to original depth
+        #diff_x_abs = cv2.convertScaleAbs(diff_x)
+        #diff_y_abs = cv2.convertScaleAbs(diff_y)
+
+        diff_x = np.float32(diff_x)
+        diff_y = np.float32(diff_y)
+
+        gradient_mag = cv2.magnitude(diff_x, diff_y)
+
+        gradient_mag = cv2.convertScaleAbs(gradient_mag)
+
+        return gradient_mag
     
     if method == 'sobel':
         grad_x = cv2.Sobel(grey_img, -1, 1, 0, 3)
@@ -26,9 +59,11 @@ def edge_detection(img, method):
         return gradient
 
 
-sobel = edge_detection(img, 'sobel')
-laplacian = edge_detection(img, 'laplacian')
+diff = edge_detection(img, 'diff')
+#sobel = edge_detection(img, 'sobel')
+#laplacian = edge_detection(img, 'laplacian')
 
-cv2.imshow('sobel', sobel)
-cv2.imshow('laplacian', laplacian)
+cv2.imshow('diff', diff)
+#cv2.imshow('sobel', sobel)
+#cv2.imshow('laplacian', laplacian)
 cv2.waitKey()
