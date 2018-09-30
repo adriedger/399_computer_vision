43bcf40540794bf0eb41760968d266fa15bcb5cf
Andre <adriedge@gmail.com>
2018-09-29 22:13:47 -0600

    visualization going on q1

--- A2/q1EdgeDetection.py
+++ A2/q1EdgeDetection.py
@@ -21,7 +21,7 @@ def edge_detection(img, method):
         #convert back to uint8 (0,255)
         gradient_mag = cv2.convertScaleAbs(gradient_mag)
 
-        return gradient_mag
+        return gradient_mag, theta
     
     if method == 'sobel':
         sobel_x = cv2.Sobel(grey_img, cv2.CV_32F, 1, 0, 3)
@@ -31,19 +31,44 @@ def edge_detection(img, method):
         
         gradient_mag = cv2.convertScaleAbs(gradient_mag)
         
-        return gradient_mag
+        return gradient_mag, theta
 
     if method == 'laplacian':
-        gradient = cv2.Laplacian(grey_img, -1, 3)
+        gradient_mag = cv2.Laplacian(grey_img, cv2.CV_32F, 3)
         
-        return gradient
+        gradient_mag = cv2.convertScaleAbs(gradient_mag)
+        
+        return gradient_mag
 
+def visualize_theta(mag, theta):
+    #create new image, assign pixel to corresponding color*normalized mag
+    row, col = mag.shape
+    out_img = np.zeros([row, col, 3], dtype=np.uint8)
+    #color = [0, 0, 0]
+    for y in range(row):
+        for x in range(col):
+            #print mag[x, y], theta[x, y]
+            if theta[x, y] <= (3.14/2):
+                color = [255, 0 ,0]
+            elif theta[x, y] <= (3.14):
+                color = [0, 255 ,0]
+            elif theta[x, y] <= (3.14*1.5):
+                color = [0, 0 ,255]
+            else:
+                color = [255, 0, 255]
+            pixel = np.multiply(color, float(mag[x, y])/255)
+            #print pixel, color, float(mag[x, y])/255
+            out_img[x, y] = pixel
+    
+    cv2.imshow('visualize_theta', out_img)
 
-diff = edge_detection(img, 'diff')
-sobel = edge_detection(img, 'sobel')
+diff_mag, diff_theta = edge_detection(img, 'diff')
+#sobel = edge_detection(img, 'sobel')
 #laplacian = edge_detection(img, 'laplacian')
 
-cv2.imshow('diff', diff)
-cv2.imshow('sobel', sobel)
+cv2.imshow('diff_mag', diff_mag)
+#cv2.imshow('sobel', sobel)
 #cv2.imshow('laplacian', laplacian)
+
+visualize_theta(diff_mag, diff_theta)
 cv2.waitKey()
