33fe4dbc567de49c2d703169136bb6a3a2ec6058
Andre <adriedge@gmail.com>
2018-10-05 14:36:00 -0600

    working on harris min supression

--- A2/q2FeatureDetectionAndMatching.py
+++ A2/q2FeatureDetectionAndMatching.py
@@ -1,12 +1,11 @@
 #Andre Driedger 1805536
 #Prof: Dana Cobzas CMPT 399 Computer Vision
-#corners are where m1,m3 are high and m2 is 0
 
 import numpy as np
 import cv2 
 
-img = cv2.imread('lena.tif')
-#img = cv2.imread('data_q2/checkboard.png')
+#img = cv2.imread('lena.tif')
+img = cv2.imread('data_q2/checkboard.png')
 cv2.imshow('input', img)
 
 def my_harris(img, k, blocksize):
@@ -21,31 +20,45 @@ def my_harris(img, k, blocksize):
     #between -1, 1, will help visualize
     #Ix = (Ix / np.amax(abs(Ix)))    
     #Iy = (Iy / np.amax(abs(Iy)))
-
-    IxIx = Ix*Ix
-    IxIy = Ix*Iy
-    IyIy = Iy*Iy
-    print np.amax(IxIx), np.amin(IxIx)
-    print np.amax(IxIy), np.amin(IxIy)
-    print np.amax(IyIy), np.amin(IyIy)
+    
+    #get II's, run a gaussian filter through them
+    IxIx = cv2.GaussianBlur(Ix*Ix, (3, 3), 0)
+    IxIy = cv2.GaussianBlur(Ix*Iy, (3, 3), 0)
+    IyIy = cv2.GaussianBlur(Iy*Iy, (3, 3), 0)
+    #print np.amax(IxIx), np.amin(IxIx)
+    #print np.amax(IxIy), np.amin(IxIy)
+    #print np.amax(IyIy), np.amin(IyIy)
         
     #need to be float to save pixels as correct data type
-    out_img = np.zeros([row, col, 3], dtype=np.uint8)
-
+    c_img = np.zeros([row, col, 1], dtype=np.float32)
+    out_img = np.zeros([row, col, 1], dtype=np.uint8)
+    
+    #calculate c for each pixel, threshold
     for y in range(3, row-3):
         for x in range(3, col-3):
-            #print Ix[y,x]
-            #print(IxIy[y:y+3, x:x+3])
+            
             m1 = np.sum(IxIx[y:y+3, x:x+3])
             m2 = np.sum(IxIy[y:y+3, x:x+3])
             m3 = np.sum(IyIy[y:y+3, x:x+3])
             c = (m1*m3-m2*m2) - 0.04*(m1+m3)
-            #if c > 0.5:
-                #print x, y, m1, m2, m3, c
-            if c > 100000000000:    
-                out_img[y, x] = [0, 0, 255]
+            #threshold
+            if c > 10000000000:
+                c_img[y, x] = c
+                #out_img[y, x] = 255
             else:
-                out_img[y, x] = [0, 0, 0]
+                c_img[y, x] = 0
+                #out_img[y, x] = 0
+    
+    #take max of adjacent c pixels, supress lower values
+    for y in range(0, row, 3):
+        for x in range(0, col, 3):
+            
+            minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(c_img[y:y+3, x:x+3]) 
+            #keep highest, supress lower, Loc returns as a Point(x, y)
+            if maxVal > 0:
+                #print minVal, maxVal, minLoc, maxLoc
+                locX, locY = maxLoc
+                out_img[y+locY, x+locX] = 255
     
     cv2.imshow('cornerness', out_img)
     cv2.waitKey()
