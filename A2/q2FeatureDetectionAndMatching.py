e3810927a0c847bb3468fe7ae8d97f81384b6cad
Andre <adriedge@gmail.com>
2018-10-05 16:19:46 -0600

    q2 harris working

--- A2/q2FeatureDetectionAndMatching.py
+++ A2/q2FeatureDetectionAndMatching.py
@@ -4,8 +4,8 @@
 import numpy as np
 import cv2 
 
-#img = cv2.imread('lena.tif')
-img = cv2.imread('data_q2/checkboard.png')
+img = cv2.imread('lena.tif')
+#img = cv2.imread('data_q2/checkboard.png')
 cv2.imshow('input', img)
 
 def my_harris(img, k, blocksize):
@@ -25,42 +25,41 @@ def my_harris(img, k, blocksize):
     IxIx = cv2.GaussianBlur(Ix*Ix, (3, 3), 0)
     IxIy = cv2.GaussianBlur(Ix*Iy, (3, 3), 0)
     IyIy = cv2.GaussianBlur(Iy*Iy, (3, 3), 0)
-    #print np.amax(IxIx), np.amin(IxIx)
-    #print np.amax(IxIy), np.amin(IxIy)
-    #print np.amax(IyIy), np.amin(IyIy)
         
     #need to be float to save pixels as correct data type
     c_img = np.zeros([row, col, 1], dtype=np.float32)
     out_img = np.zeros([row, col, 1], dtype=np.uint8)
     
     #calculate c for each pixel, threshold
-    for y in range(3, row-3):
-        for x in range(3, col-3):
+    for y in range(row-3):
+        for x in range(col-3):
             
             m1 = np.sum(IxIx[y:y+3, x:x+3])
             m2 = np.sum(IxIy[y:y+3, x:x+3])
             m3 = np.sum(IyIy[y:y+3, x:x+3])
             c = (m1*m3-m2*m2) - 0.04*(m1+m3)
-            #threshold
+            #threshold, assign cornerness to middle pixel
             if c > 10000000000:
-                c_img[y, x] = c
+                c_img[y+1, x+1] = c
                 #out_img[y, x] = 255
             else:
-                c_img[y, x] = 0
+                c_img[y+1, x+1] = 0
                 #out_img[y, x] = 0
     
-    #take max of adjacent c pixels, supress lower values
-    for y in range(0, row, 3):
-        for x in range(0, col, 3):
+    #take max of snapshot of c pixels, supress lower values
+    for y in range(0, row, 20):
+        for x in range(0, col, 20):
             
-            minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(c_img[y:y+3, x:x+3]) 
+            minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(c_img[y:y+20, x:x+20]) 
             #keep highest, supress lower, Loc returns as a Point(x, y)
             if maxVal > 0:
                 #print minVal, maxVal, minLoc, maxLoc
                 locX, locY = maxLoc
                 out_img[y+locY, x+locX] = 255
+                img[y+locY-3:y+locY+3, x+locX-3:x+locX+3] = [0, 0, 255]
     
     cv2.imshow('cornerness', out_img)
+    cv2.imshow('features', img)
     cv2.waitKey()
 
 my_harris(img, 0, 9)
