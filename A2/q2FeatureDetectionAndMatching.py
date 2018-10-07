edd74764d8d920122e88bda144083d4b11db5c8f
Andre <adriedge@gmail.com>
2018-10-06 21:47:25 -0600

    a2q2 updates

--- A2/q2FeatureDetectionAndMatching.py
+++ A2/q2FeatureDetectionAndMatching.py
@@ -17,10 +17,6 @@ def my_harris(img, k, blocksize):
     Ix = cv2.Sobel(grey_img, cv2.CV_32F, 1, 0, 3)
     Iy = cv2.Sobel(grey_img, cv2.CV_32F, 0, 1, 3)
     
-    #between -1, 1, will help visualize
-    #Ix = (Ix / np.amax(abs(Ix)))    
-    #Iy = (Iy / np.amax(abs(Iy)))
-    
     #get II's, run a gaussian filter through them
     IxIx = cv2.GaussianBlur(Ix*Ix, (3, 3), 0)
     IxIy = cv2.GaussianBlur(Ix*Iy, (3, 3), 0)
@@ -41,19 +37,16 @@ def my_harris(img, k, blocksize):
             #threshold, assign cornerness to middle pixel
             if c > 10000000000:
                 c_img[y+1, x+1] = c
-                #out_img[y, x] = 255
             else:
                 c_img[y+1, x+1] = 0
-                #out_img[y, x] = 0
     
-    #take max of snapshot of c pixels, supress lower values
+    #keep the max of sections of c_img, supress lower values
     for y in range(0, row, 20):
         for x in range(0, col, 20):
             
             minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(c_img[y:y+20, x:x+20]) 
-            #keep highest, supress lower, Loc returns as a Point(x, y)
+            #keep highest, Loc returns as a Point(x, y)
             if maxVal > 0:
-                #print minVal, maxVal, minLoc, maxLoc
                 locX, locY = maxLoc
                 out_img[y+locY, x+locX] = 255
                 img[y+locY-3:y+locY+3, x+locX-3:x+locX+3] = [0, 0, 255]
