864fac5813e39b964c20926c20bd82f1f9a4f2cb
Andre <adriedge@gmail.com>
2018-10-10 15:44:32 -0600

    started A3

---
+++ A3/q1ImageRectification.py
@@ -0,0 +1,13 @@
+#Andre Driedger 1805536
+#Prof: Dana Cobzas CMPT 399 Computer Vision
+
+import numpy as np
+import cv2 
+
+img = cv2.imread('data_q1_rect/book1.jpg', 0)
+cv2.imshow('input', img)
+
+def get_corners(im):
+    return corners
+
+
