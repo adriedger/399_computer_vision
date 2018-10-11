2646eed4aab6f7a208338f60ca8a251c0c12b30f
Andre <adriedge@gmail.com>
2018-10-11 12:42:55 -0600

    q1 get_corners implemented

--- A3/q1ImageRectification.py
+++ A3/q1ImageRectification.py
@@ -5,9 +5,32 @@ import numpy as np
 import cv2 
 
 img = cv2.imread('data_q1_rect/book1.jpg', 0)
-cv2.imshow('input', img)
 
-def get_corners(im):
-    return corners
+def get_corners(img):
+    
+    print "Double-click on the four corners of the quadrilateral"
+    pts = []
+    # mouse callback function, runs this function on mouse double-click
+    def mouse_dblclk(event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDBLCLK: 
+            pts.append((x, y))
+            cv2.circle(img, (x,y), 25, 0, -1)
+    
+    cv2.namedWindow('input')
+    cv2.setMouseCallback('input', mouse_dblclk)
+    #runs until there are 4 double-clicks   
+    while 1:
+        cv2.imshow('input', img)
+        if cv2.waitKey(20) and len(pts) == 4:
+            break
+    
+    return np.asarray(pts)
 
+def get_affine(pts1, pts2):
+    #shape 2x4 pts1 into 6x2n
+
+    return 
+
+pts = get_corners(img)
+print pts
 
