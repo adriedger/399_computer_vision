32a88c2446bea3f900151c79700a6b45b3ffa62f
Andre <adriedge@gmail.com>
2018-10-11 14:12:58 -0600

    q1 get_affine done

--- A3/q1ImageRectification.py
+++ A3/q1ImageRectification.py
@@ -18,7 +18,7 @@ def get_corners(img):
     
     cv2.namedWindow('input')
     cv2.setMouseCallback('input', mouse_dblclk)
-    #runs until there are 4 double-clicks   
+    # runs until there are 4 double-clicks   
     while 1:
         cv2.imshow('input', img)
         if cv2.waitKey(20) and len(pts) == 4:
@@ -27,10 +27,30 @@ def get_corners(img):
     return np.asarray(pts)
 
 def get_affine(pts1, pts2):
-    #shape 2x4 pts1 into 6x2n
 
-    return 
+    # shape 2x4 pts1 into 6x2n
+    # reminder: dimensions are y,x for numpy
+    row, col = pts1.shape
+    M = np.zeros([2*row, 6], dtype=np.float32)
+    #print M
+    for y in range(row):
+        M[2*y, 0:2] = pts1[y, :]
+        M[2*y, 2] = 1
+        M[2*y+1, 3:5] = pts1[y, :]
+        M[2*y+1, 5] = 1
+    
+    #print M
+    # shape 2x4 pts2 into 1x2n
+    N = pts2.reshape((8, 1))
+    #print N
 
-pts = get_corners(img)
-print pts
+    # use least-squares (since M isn't square) to calculate determinants
+    x = np.linalg.lstsq(M, N)
+    #print x[0]
+    return np.reshape(x[0], (2, 3))
 
+#pts = get_corners(img)
+a = np.arange(1, 9).reshape((4, 2))
+b = np.arange(-9, -1).reshape((4, 2))
+print get_affine(a, b)
+#print cv2.getAffineTransform(a, b)
