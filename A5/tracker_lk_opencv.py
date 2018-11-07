36c7cfd4f85e9541a27fa473fa125570b715e3a3
Andre <adriedge@gmail.com>
2018-11-06 19:07:21 -0700

    working a5q1

--- A5/tracker_lk_opencv.py
+++ A5/tracker_lk_opencv.py
@@ -1,3 +1,6 @@
+#Andre's version
+
+from __future__ import print_function
 import sys
 import cv2
 import numpy as np
@@ -66,42 +69,49 @@ class Tracker:
         if len(self.curr_img.shape) == 3:
             self.curr_img = cv2.cvtColor(self.curr_img, cv2.COLOR_BGR2GRAY)
 
-        # gaussian filter and derivatives 
-        # self.curr_img = ...
-        # dx = ...
-        # dy = ...
+        # gaussian filter and derivatives
+        self.curr_img = cv2.GaussianBlur(self.curr_img, (self.gauss_kernel_size, self.gauss_kernel_size), 0)
+        dx = cv2.Sobel(self.curr_img, cv2.CV_32F, 1, 0, 3)
+        dy = cv2.Sobel(self.curr_img, cv2.CV_32F, 0, 1, 3)
+        #print(dx, dy)
 
         # update tracker until convergence of until max iterations 
-        for it in range(self.max_iters):
+        for i in range(self.max_iters):
 
             # get curent location of tracker from self.curr_rect
-            #x = ...
-            #y = ...
-            #w = ...
-            #h = ...
+            x, y, w, h = self.curr_rect
+            #print(x, y, w, h)
 
             # blocks of derivatives and image using at subpixel accuracy
-            # because the tracker is updated at subpizel accuracy we need to use interpolation
+            # because the tracker is updated at subpixel accuracy we need to use interpolation
             # use this function cv2.getRectSubPix            
             # size of the tracker (for the interpolatin function 
             rect_center = (x + w / 2.0, y + h / 2.0)
-            rect_size = (int(w), int(h))
-            # curW = ...
-            # dxw = ...
-            # dyw = ...
+            rect_size = (int(w), int(h))            
+            curW = cv2.getRectSubPix(self.curr_img, rect_size, rect_center)
+            dxw = cv2.getRectSubPix(dx, rect_size, rect_center)
+            dyw = cv2.getRectSubPix(dy, rect_size, rect_center)
+            #print(curW.shape, dxw.shape, dyw.shape)
 
-            # downsample for better efficiency
+            # downsample for better efficiency (skips every 3)
             curW = curW[::skip, ::skip]
             dxw = dxw[::skip, ::skip]
             dyw = dyw[::skip, ::skip]
-
-            # gt difference with template 
-            # diff = ...
+            #print(curW.shape, dxw.shape, dyw.shape, self.template.shape)
+            
+            # get difference with template 
+            #diff = cv2.subtract(curW, self.template)
+            #diff = cv2.subtract(self.template, curW, dtype=cv2.CV_32F)
+            diff = cv2.subtract(curW, self.template, dtype=cv2.CV_32F)
+            #print(diff.dtype)
+            #diff = curW - self.template
+            #print(diff, curW - self.template)
 
             # reshape derivative and difference blocks as columns 
-            #Idx = ...
-            #Idy = ...
-            #Idt = ...
+            Idx = dxw.reshape((-1, 1))
+            Idy = dyw.reshape((-1, 1))
+            Idt = diff.reshape((-1, 1))
+            #print(Idx, Idy, Idt.shape)
 
             # if some values end up undefined ...
             Idx[np.isnan(Idx)] = 0.0
@@ -109,8 +119,9 @@ class Tracker:
             Idt[np.isnan(Idt)] = 0.0
 
             # make M matrix by concatenating Idx Idy
-            # M = ...
-
+            M = np.concatenate((Idx, Idy), axis=1)
+            #print(M.shape)
+            #return
             # solve equation system to get u,v
             UV = -np.dot(np.linalg.pinv(M), Idt)
             UV[np.isnan(UV)] = 0.0
